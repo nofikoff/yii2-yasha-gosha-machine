d00e6811abc0ce092b874c4d500e9b89a06cc1c8
nofikoff <ruslan.novikov@gmail.com>
2016-08-25 21:06:58 +0300

    Add files via upload

---
+++ GoogleMachine.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * by Novikov.ua 2016
+ */
+
+
+namespace app\models;
+
+//use Yii;
+
+class GoogleMachine
+{
+
+    public $gdomain = 'google.ru';
+    public $glang = 'ru';
+    public $gnearcity = '';
+
+    // режим ТОЧНОЕ СООТВЕТСВИЕ в кавычкаъ
+    public $exactly = false;
+    // мобильная версия поисковика (прост как идея)
+    public $mobile_version = false;
+    // мнять БЕСПОЛЕЗНО гугл выкупает
+    public $noJs = true;
+
+
+
+    public function get_page($strReq)
+    {
+        $browser= new Yii2Proxybrowser();
+        return $browser->get_http($this->get_url_request($strReq));
+    }
+
+
+    private function get_url_request($strReq)
+    {
+        $strReq = trim($strReq);
+        if ($this->exactly){
+            $strReq = '"'.$strReq.'"';
+        }
+        $strUrl='https://www.'.$this->gdomain.'/search?q='. urlencode($strReq);
+        if ($this->noJs) $strUrl.='&gbv=1';
+        return $strUrl;
+
+    }
+
+}
